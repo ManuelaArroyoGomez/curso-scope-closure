45a8c8e29153f8785db5530aa494ae24d5a56df2
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-10-15 18:19:53 -0500

    nuevos documentos js

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; //undefined
+firstName = 'Manuela'; //tiene un valor
+console.log(firstName);
+
+var lastName = 'Arroyo'; //declarar y asignar
+lastName = 'Gomez'; //reasignar
+console.log(lastName);
+
+var secondName = 'Rafael'; //declarar y asignar
+var secondName = 'Eduardo'; //reasignamos
+console.log(secondName);
+
+// let
+let fruit = 'apple'; //declarar y asignar
+fruit = 'kiwi'; //reasignar
+console.log(fruit);
+
+// let fruit = 'banana' no se puede redeclarar 
+
+// const 
+const animal = 'dog'; //declara y asigna
+// animal = 'cat' no se puede reasignar ni redeclarar 
+console.log(animal); 
+
+const vehicles = [];
+vehicles.push("🚗"); //Se le agrega el emoji al conjunto vacio
+console.log(vehicles);
+
+vehicles.pop(); //se vuelve vacio el vehicles
+console.log(vehicles);
