4d5718b931724e4cec465868f2ff9a726ea3ddab
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2024-10-15 19:10:58 -0500

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+// var nameOfDog; //undefined
+console.log(nameOfDog);
+var nameOfDog = 'Bruno';
+console.log(nameOfDog);
+
+// var bruno; // undefined
+// function nameOfPet() {
+//     console.log(`El mejor perrito es ${bruno}`);
+// }
+nameOfPet();
+function nameOfPet() {
+    console.log(`El mejor perrito es ${bruno}`);
+}
+// var bruno = 'Brunito';
